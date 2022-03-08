4e46b307626513b1fae0e451358a7e50ebced112
Christian Harraeus <christian@harraeus.de>
2022-03-08 20:38:27 +0100

    Shorten the reporting point names to 5 characters
    * Max. 5 characters long reporting point names
    * Add airport dictionary which contains all reporting point names per airport
    * Some refactoring

--- src/ofmx_data.py
+++ src/ofmx_data.py
@@ -44,14 +44,6 @@ import xml.etree.ElementTree as ET
 
 import settings
 
-# List of reporting point, sorted by region, airport, reporting point id
-class ReportingPointClass(list):
-    def __init__(self, rp_data: list) -> None:
-        _ofmx_data_line = rp_data
-
-    def __repr__(self) -> str:
-        return None
-
 ReportingPointList: list = []
 
 class OFMXFileClass:
@@ -71,10 +63,8 @@ class OFMXFileClass:
             exit()
         ## Root element of the XML/OFMX tree
         self._tree_root = self._xml_tree.getroot()
-
-        #self._airport_dict: dict = {}
-        ## Meta data about the OFMX file
-        self._OFMX_meta_data: dict = {}
+        ## Airport dictionary with all reporting points per airport
+        self._airport_dict = {}
 
     @property
     def OFMX_meta_data(self) -> dict:
@@ -99,39 +89,54 @@ class OFMXFileClass:
             # Available types: ['VFR-RP', 'VFR-MRP', 'VFR-HELI', 'VFR-GLDR', 'ICAO']
             # @todo implement filter
             if (dpn.find('codeType') is not None) and \
-               (dpn.find('codeType').text in ['VFR-RP', 'VFR-MRP', 'VFR-HELI']):
-                rp_data.append(dpn.find('codeType').text)   # RP-type
+               (dpn.find('codeType').text.strip() in ['VFR-RP', 'VFR-MRP', 'VFR-HELI']):
+                # store reporting point type
+                rp_data.append(dpn.find('codeType').text.strip())
+                # store ICAO airport code
                 if dpn.find('AhpUidAssoc/codeId') is not None:
-                    rp_data.insert(0, dpn.find('AhpUidAssoc/codeId').text)  # ICAO airport code
+                    rp_data.insert(0, dpn.find('AhpUidAssoc/codeId').text.strip())
                 else:
                     rp_data.insert(0, 'n/a ')
-                if dpn.find('txtName') is not None:    
-                    rp_data.append(dpn.find('txtName').text)   # RP-Name
+                # store reporting point name
+                if dpn.find('txtName') is not None:
+                    rp_data.append(dpn.find('txtName').text.strip())
                 else:
                     rp_data.append('n/a')
                 # Find the DpnUid element within the Dpn knot and read the data
                 dpn_uid = dpn.find('DpnUid')
                 if dpn_uid is not None:
-                    rp_data.insert(1, dpn_uid.find('codeId').text)    # RP-Id
-                    # Coordinates (RP-longitude and RP-latitude): RP-coordinates
+                    # store reporting point id
+                    rp_data.insert(1, dpn_uid.find('codeId').text.strip())
+                    # store coordinates (RP-longitude and RP-latitude)
                     rp_data.append(self.convert_to_xplane_coord([dpn_uid.find('geoLat').text, 
                                                                  dpn_uid.find('geoLong').text]))
-                    rp_data.insert(0, dpn_uid.attrib.get('region'))   # RP-Region
+                    # reporting point region
+                    rp_data.insert(0, dpn_uid.attrib.get('region'))
                 else:
                     rp_data.insert(1, '')     # RP-Id
                     rp_data.append(['', ''])  # RP coordinates
                     rp_data.insert(0, '')     # RP region
 
+                # Create a reporting point id which is only five
+                # characters long and unique within one airport
+                rp_data.append(self.build_rp_name5(rp_data))
+
+                # Add shortened region
+                rp_data.append(rp_data[0][0:2])
+
                 # Add reporting point data to reporting point list
                 ReportingPointList.append(rp_data)
+
+                #print(self._airport_dict)
+
                 if self.__settings_object.verbose:
                     print(rp_data)
 
+        # Sort reporting point list by region, icao-id and reporting 
+        # point id
         ReportingPointList.sort(key=lambda rp_data: self.rp_sortkey(rp_data))
 
-    # Sort reporting point list by region, icao-id and reporting 
-    # point id
-    def rp_sortkey(self, rp_data: ReportingPointClass) -> str:
+    def rp_sortkey(self, rp_data: list) -> str:
         """
         Return the the sort key for the ReportingPoints list
         
@@ -155,18 +160,15 @@ class OFMXFileClass:
         returns
             [lat, long] list with coordinates for X-Plane 11
         """
-        xplane_coord = []
-        # Latitude: if SOUTH then convert to negative value
-        if ofmx[0].endswith('S'):
-            ofmx[0] = '-' + ofmx[0]
-        # cut last character (N, S)
-        xplane_coord.append(ofmx[0][0:len(ofmx[0]) - 1])
-        # Longitude: if WEST then convert to negative value
-        if ofmx[0].endswith('W'):
-            ofmx[1] = '-' + ofmx[1]
-        # cut last character (W, E)
-        xplane_coord.append(ofmx[1][0:len(ofmx[1]) - 1])
-        return xplane_coord
+        lat, long = ofmx
+        if lat.endswith('S'):
+            # Latitude: if SOUTH then convert to negative value
+            lat = '-' + lat
+        if long.endswith('W'):
+            # Longitude: if WEST then convert to negative value
+            long = '-' + long
+        # cut last character (N, S, W, E) and return the coord as list
+        return [lat[0:len(lat) - 1], long[0:len(long) - 1]]
 
     def get_reporting_point(self) -> list:
         """Generator function to return the ofmx data lines
@@ -176,3 +178,138 @@ class OFMXFileClass:
         """
         for rp in ReportingPointList:
             yield rp
+
+    def build_rp_name5(self, rp_data: list) -> str:
+        """
+        rename reporting point ids to max len of 5 characters
+
+        X-Plane 11 only allows navigation point designatores with a
+        maximum length of five characters. So all reporting point ids
+        have to be shortened to max. five characters length.  
+
+        If there are reporting point which id is longer than five
+        characters or not unique within one ariport the id must be 
+        rebuilt.  
+        e.g.  
+        The reporting point ids AUTOBAHN-OST and AUTOBAHN-WEST are
+        replaced by AB-O and AB-W.  
+
+        Args:
+            rp_data (list): all data available about the reporting point
+
+        Returns:
+            str: the new reporting point id
+        """
+        rp_region, rp_airport, rp_id_old, rp_type, rp_name, *other = rp_data
+        rp_id_old = rp_id_old.replace('-', ' ')
+        rp_id_words = rp_id_old.split()
+        rp_id = rp_id_old[:]
+
+        # only keep the last two words
+        while len(rp_id_words) > 2:
+            del rp_id_words[0]
+        #print(f'los gehts: {rp_id_words=}, {rp_id=}')
+
+        # Rule 1:
+        # if reporting point like E, N1, N2
+        # replace it with the rp name like ECHO, NOVE1, ...
+        if len(rp_id_words[0]) <= 2:
+            rp_id = rp_id_words[0].strip()
+            if len(rp_id) == 1:
+                rp_id = rp_name[0:5]
+            elif len(rp_id) == 2 \
+                    and rp_id[1] in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
+                    #e.g. ECHO 2, NOVEMBER 1
+                    rp_id = rp_name[0:4] + rp_name[-1]
+            #print(f'Rule 1: {rp_id=}\n')
+        else:
+            while len(rp_id) > 5:
+
+                # Rule 2:
+                # if first word starts with 'AUTOBAHN',
+                # replace the string 'AUTOBAHN' by 'AB'
+                # e.g.  
+                # AUTOBAHN OST  --> AB OST  
+                # AUTOBAHN WEST --> AB WEST  
+                # AUTBAHNKNOTEN --> ABKNOTEN
+                if rp_id_words[0].startswith('AUTOBAHN'):
+                    #print(f'    Rule 2: {rp_id_words=}, {rp_id_words[0]=}')
+                    rp_id_words[0] = rp_id_words[0].replace('AUTOBAHN', 'AB')
+                    #print(f'    Rule 2e: {rp_id_words}, {rp_id_words[0]=}\n')
+
+                # Rule 3:
+                # rp_id_old starts with 'ST.' or 'ST. ' or 'ST ',
+                # remove this
+                elif rp_id_words[0] in ['ST.', 'ST']:
+                    del rp_id_words[0]
+                
+                # Rule 4:
+                # rp_id_old ends with 'NORD', 'SÜD', 'WEST', 'OST',  
+                #                     'NORWEST', 'NORDOST', 'SÜDWEST'  
+                #                      or 'SÜDOST',  
+                # replace this by 'N', 'S', 'W', 'O', 'NW', 'NO', 'SW'  
+                #                  or 'SO'  
+                # e.g.  
+                # AB WEST --> AB W  
+                # AMSTETTEN WEST --> AMSTETTEN W  
+                elif rp_id_words[-1] in ['NORD', 'SÜD', 'WEST', 'OST']:
+                    match rp_id_words[-1]:
+                        case 'NORD':
+                            rp_id_words[-1] = '-N'
+                        case 'SÜD':
+                            rp_id_words[-1] = '-S'
+                        case 'WEST':
+                            rp_id_words[-1] = '-W'
+                        case 'OST':
+                            rp_id_words[-1] = '-O'
+
+                # Rule 5:
+                # rp-id has two words:  
+                # shorten the first of two remaining words to three
+                #     characters;  
+                # if the last word has more than one character:  
+                #     shorten the first word to three characters
+                #     shorten the last word to two characters  
+                #     e.g.  
+                #     AMSTETTEN W --> AMS W  
+                #     AMSTETTEN PARKPLATZ --> AMSPA  
+                elif len(rp_id_words) == 2:
+                    #print(f'    Rule 5: {rp_id_words=}, {rp_id=}')
+                    e0 = min(3, len(rp_id_words[0]))
+                    e1 = 5 - e0
+                    rp_id = rp_id_words[0][:e0] + rp_id_words[1][:e1]
+                    #print(f'    Rule 5e: {rp_id_words=}, {rp_id=}\n')
+                
+                # Rule 6:
+                # the rp_id is still > 5 characters long and the rules
+                # above do not apply:
+                # shorten the rp-id to max. five characters  
+                # e.g.  
+                # LORENZEN --> LOREN  
+                else:
+                    rp_id = rp_id_words[0][:5]
+
+        # eliminate non unique reporting point names (within one airport)
+        # and add the reporting point name to airport dictionary
+        count = 0
+        is_unique_name = False
+        if rp_data[1] in self._airport_dict:
+            while not is_unique_name:
+                if rp_id in self._airport_dict[rp_data[1]]:
+                    # reporting point with same name is already existing
+                    e = min(4, len(rp_id) - (1 if count > 0 else 0))
+                    count += 1
+                    rp_id = rp_id[:e] + str(count)
+                else:
+                    is_unique_name = True
+        self.add_reporting_point(rp_data[1], rp_id)
+        return rp_id
+
+    def add_reporting_point(self, rp_airport: str, rp_name5: str) -> None:
+        """add a reporting point to the airport"""
+        if rp_airport in self._airport_dict:
+            rp_list = self._airport_dict[rp_airport]
+        else:
+            rp_list = []
+        rp_list.append(rp_name5)
+        self._airport_dict[rp_airport] = rp_list
--- src/xplane_navdata.py
+++ src/xplane_navdata.py
@@ -28,53 +28,34 @@ class XPlaneNavDataClass:
         ## Settings information
         self.__settings_: SettingsClass = impfix_settings
         ## File object for existing user_fix.dat file
-        self.__xplane_user_fix_dat_file = None
+        self._xplane_user_fix_dat_file = \
+            self._open_user_fix_dat_file(impfix_settings.xplane_user_fix_dat_filename)
         ## File object for new user_fix.dat.impfix file
-        self.__new_user_fix_dat_file = None
+        self._new_user_fix_dat_file = \
+            self._create_new_user_fix_dat_file(impfix_settings.new_user_fix_dat_filename)
 
-    def write_new_user_fix_dat_file(self, ofmx_data: OFMXFileClass) -> None:
+    def _create_new_user_fix_dat_file(self, filename):
+        """Create the **new** user_fix.dat file
+
+        Create new user_fix.dat file as 'user_fix.dat.impfix'. An 
+        existing user_fix.dat.impfix file will be overwritten
+        without warning.
         """
-        Create new user_fix.dat file. 
-
-        1. Create new user_fix.dat file as 'user_fix.dat.impfix'. An 
-           existing user_fix.dat.impfix file will be overwritten
-           without warning.
-        2. Copy all none-Impfix-generated data into the new file:
-             => copy all lines until the start mark is found.
-        3. Generate and write new start mark into the new user_fix.dat
-           file.
-        4. Write the Open Flight Map data into the new user_fix.dat 
-           file.
-        5. Generate the new end mark and copy the rest of the old 
-           user_fix.dat file.
-        6. If necessary write the X-Plane end-of-file mark into 
-           the new user_fix.dat file.
-        """
-
-        xplane_userfix_dat_eof: str = '99'  # @todo noch in settings aufnehmen
-        eof_mark_found: bool = False
-        end_mark_found: bool = False
-        start_mark_found: bool = False
-        ofm_data_written: bool = False
-
-        # Create the new user_fix.dat file
         try:
-            self.__new_user_fix_dat_file = \
-                open(self.__settings_.new_user_fix_dat_filename, 'w')
+            return open(filename, 'w')
         except IOError as e:
             errno, strerror = e.args
-            print('**I/O error({0}): {1}'.format(errno,strerror))
-            print('**Error while creating the file \'{0}\''
-                .format(self.__settings_.new_user_fix_dat_filename))
+            print('**I/O error({}): {}'.format(errno, strerror))
+            print('**Error while creating the file \'{}\''.format(filename))
             exit()
         except:
             print('**Unknown error:\n', sys.exc_info()[0:2])
             exit()
 
-        # Open the existing user_fix.dat file
+    def _open_user_fix_dat_file(self, filename):
+        """Open the **existing** user_fix.dat file"""
         try:
-            self.__xplane_user_fix_dat_file = \
-                open(str(self.__settings_.xplane_user_fix_dat_filename), 'r')
+            return open(str(filename), 'r')
         except IOError as e:
             errno, strerror = e.args
             print('**I/O error({0}): {1}'.format(errno,strerror))
@@ -85,10 +66,33 @@ class XPlaneNavDataClass:
             print('**Unknown error:\n', sys.exc_info()[0:2])
             exit()
 
+
+    def write_new_user_fix_dat_file(self, ofmx_data: OFMXFileClass) -> None:
+        """
+        Create new user_fix.dat file. 
+
+        1. Copy all none-Impfix-generated data into the new file:
+             => copy all lines until the start mark is found.
+        2. Generate and write new start mark into the new user_fix.dat
+           file.
+        3. Write the Open Flight Map data into the new user_fix.dat 
+           file.
+        4. Generate the new end mark and copy the rest of the old 
+           user_fix.dat file.
+        5. If necessary write the X-Plane end-of-file mark into 
+           the new user_fix.dat file.
+        """
+
+        xplane_userfix_dat_eof: str = '99'  # @todo noch in settings aufnehmen
+        eof_mark_found: bool = False
+        end_mark_found: bool = False
+        start_mark_found: bool = False
+        ofm_data_written: bool = False
+
         # Copy data from old into new user_fix.dat file and insert ofmx-data lines
         cur_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S:%s')
         print('Copying old non-impfix data into new file ', end='')
-        for userfix_dat_line in self.__xplane_user_fix_dat_file:
+        for userfix_dat_line in self._xplane_user_fix_dat_file:
             if (not start_mark_found) \
                and (userfix_dat_line.startswith(self.__ImpfixStartMark)):
                 # Start mark found => => skip all lines until the end mark
@@ -104,31 +108,27 @@ class XPlaneNavDataClass:
                 eof_mark_found = True
             if (start_mark_found or eof_mark_found) and (not ofm_data_written):
                 # Write new start mark
-                self.__new_user_fix_dat_file.write(self.__ImpfixStartMark 
+                self._new_user_fix_dat_file.write(self.__ImpfixStartMark 
                         + 'XXXX' + ' ' + cur_datetime + '\n')
-                self.__new_user_fix_dat_file.write('; '
-                    'effective: {0}\n'.format(ofmx_data.OFMX_meta_data.get('effective')))
+                self._new_user_fix_dat_file.write('; '
+                    'effective: {}\n'.format(ofmx_data.OFMX_meta_data.get('effective')))
                 # get and write OFMX data
                 reporting_point = ofmx_data.get_reporting_point()
                 try:
                     if self.__settings_.verbose:
                         print('\nWriting new OFM data\n', end='')
                     while True:
-                        # write data to file
-                        rp = next(reporting_point)
-                        # Coords
-                        line = '\t{}\t{}'.format(rp[5][0], rp[5][1])
-                        # reporting point id
-                        line = line + '\t{}'.format(rp[2][0:5])
-                        # airport icao id
-                        line = line + '\t\t{}'.format(rp[1])
-                        # region
-                        line = line + '\t{}\n'.format(rp[0][0:2])
-                        self.__new_user_fix_dat_file.write(line)
+                        # get the data
+                        rp_region, rp_airport, rp_id, *other, coords, rp_id5, rp_region2 = next(reporting_point)
+                        lat, long = coords
+                        # build the line to be written
+                        self._new_user_fix_dat_file.write('\t{}\t{}'.format(lat, long))
+                        self._new_user_fix_dat_file.write('\t{}'.format(rp_id5))
+                        self._new_user_fix_dat_file.write('\t\t{}'.format(rp_airport))
+                        self._new_user_fix_dat_file.write('\t{}'.format(rp_region2))
+                        self._new_user_fix_dat_file.write('\n')
                         if self.__settings_.verbose:
-                            print(line, end='')
-                        # Get next ofmx data line
-                        #ofmx_data_line = next(ofmx_data_generator)
+                            print(rp_region, rp_airport, rp_id, '-->', rp_id5)
 
                 except StopIteration:
                     ofm_data_written = True
@@ -136,7 +136,7 @@ class XPlaneNavDataClass:
                     print('**Unexpected error:\n', sys.exc_info()[0:2])
                     exit()
                 # Write new end mark
-                self.__new_user_fix_dat_file.write(self.__ImpfixEndMark 
+                self._new_user_fix_dat_file.write(self.__ImpfixEndMark 
                         + 'XXXX' + ' ' + cur_datetime + '\n')
                 print('\nOFM data successfully written\nCopying rest of original file ', end='')
 
@@ -144,18 +144,18 @@ class XPlaneNavDataClass:
             # but do not copy the lines with the start and end marks,
             # and do not copy all the lines between the start and end mark lines
             if (not start_mark_found) or eof_mark_found:
-                self.__new_user_fix_dat_file.write(userfix_dat_line)
+                self._new_user_fix_dat_file.write(userfix_dat_line)
             if self.__settings_.verbose:
                 print('.', end='')
 
         if not eof_mark_found:
             # No end-of-file mark found (should not occur, but who knows...)
             # Just add the end-of-file mark
-            self.__new_user_fix_dat_file.write(xplane_userfix_dat_eof + '\n')
+            self._new_user_fix_dat_file.write(xplane_userfix_dat_eof + '\n')
             print('**Warning: Missing End-of-File mark.\n... added.')
 
         # Close files
-        self.__new_user_fix_dat_file.close()
-        self.__xplane_user_fix_dat_file.close()
+        self._new_user_fix_dat_file.close()
+        self._xplane_user_fix_dat_file.close()
         print('\nNew File successfully created and written')
 
